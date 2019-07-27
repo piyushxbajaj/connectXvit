de617ed949508457c35d94fe5181dc53ad5bdcf6
PiyushxBajaj <piyushpiyush41@gmail.com>
2019-07-27 15:09:55 +0530

    button added

---
+++ connectXvit/connectXvit/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  connectXvit
+//
+//  Created by Piyush Bajaj on 7/27/19.
+//  Copyright © 2019 Piyush Bajaj. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
