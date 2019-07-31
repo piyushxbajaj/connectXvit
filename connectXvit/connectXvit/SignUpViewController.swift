282b77d48866d980963d3c6f4fabab4d7f9ce10b
PiyushxBajaj <piyushpiyush41@gmail.com>
2019-07-31 22:49:33 +0530

    UserDefaults added

--- connectXvit/connectXvit/SignUpViewController.swift
+++ connectXvit/connectXvit/SignUpViewController.swift
@@ -9,12 +9,26 @@
 import Cocoa
 
 class SignUpViewController: NSViewController {
-    @IBOutlet weak var rollNum: NSTextField!
+    //Outlets
+    @IBOutlet weak var rollNumField: NSTextField!
+    @IBOutlet weak var passField: NSTextField!
     
-    @IBOutlet weak var pass: NSTextField!
+    let defaults = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
+        
+        
     }
     
+    @IBAction func signUpDidPress(_ sender: NSButton) {
+        if (rollNumField.stringValue.count == 0 || passField.stringValue.count == 0){
+            print("Something is empty,WRITE")
+        }
+        else{
+            defaults.set(rollNumField.stringValue, forKey: "rollNum")
+            defaults.set(passField.stringValue,forKey: "pass")
+        }
+    }
 }
